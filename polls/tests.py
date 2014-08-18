698f01535795e3859bba45ecd2b2c4b766cae47e
brandonclosson <bclosson@gmail.com>
2014-08-18 12:19:05 -0400

    added some tests

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.utils import timezone
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+from polls.models import Poll
+
+def create_poll(question, days):
+	return Poll.objects.create(question=question, 
+		pub_date=timezone.now() + datetime.timedelta(days=days))
+
+class PollMethodTests(TestCase):
+	def test_was_published_recently_with_future_poll(self):
+		future_poll = Poll(pub_date=timezone.now() + datetime.timedelta(days=30))
+		self.assertEqual(future_poll.was_published_recently(), False)
+
+	def test_was_published_recently_with_old_poll(self):
+		old_poll = Poll(pub_date=timezone.now() - datetime.timedelta(days=30))
+		self.assertEqual(old_poll.was_published_recently(), False)
+
+	def test_was_published_recently_with_recent_poll(self):
+		recent_poll = Poll(pub_date=timezone.now() - datetime.timedelta(hours=1))
+		self.assertEqual(recent_poll.was_published_recently(), True)
+
+class PollViewTests(TestCase):
+	def test_index_view_with_no_polls(self):
+		response = self.client.get(reverse('polls:index'))
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, "No polls are available.")
+		self.assertQuerysetEqual(response.context['latest_poll_list'], [])
+
+	def test_index_view_with_a_past_poll(self):
+		create_poll(question="Some question", days=-30)
+		response = self.client.get(reverse('polls:index'))
+		self.assertQuerysetEqual(
+			response.context['latest_poll_list'],
+			['<Poll: Some question>']
+		)
+
+	# def test_index_with_a_future_poll(self):
+	# 	create_poll(question="Future Poll", days=30)
+		
--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.template import RequestContext, loader
 from django.views import generic
+from django.utils import timezone
 
 from polls.models import Poll, Choice
 
@@ -12,7 +13,9 @@ class IndexView(generic.ListView):
 	context_object_name = 'latest_poll_list'
 
 	def get_queryset(self):
-		return Poll.objects.order_by('-pub_date')[:5]
+		return Poll.objects.filter(
+			pub_date__lte=timezone.now()
+		).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
 	model = Poll
